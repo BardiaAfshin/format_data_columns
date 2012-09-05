8281a83d9c03237cea398fcef93a59bb89f5d0e5
unknown <brandonusa@gmail.com>
2012-09-05 15:49:18 -0700

    main file updated for demo run

--- class.main.php
+++ class.main.php
@@ -5,6 +5,8 @@ require_once('./classes/class.parser.php');
 $myParser = new parser('./data/w_data.data');
 $returned_data = $myParser->get_parsed_data();
 
+print_r($returned_data);
+
 foreach($returned_data as $row)
 {
     foreach($row as $column)
