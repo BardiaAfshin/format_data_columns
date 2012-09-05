38787c0dd69b45a2bef44f079e90841c70f3f2b4
Bardia Afshin <brandonusa@gmail.com>
2012-09-05 20:40:13 +0000

    adding setup.sh

--- setup.sh
+++ setup.sh
@@ -1,2 +1,10 @@
 pear config-set auto_discover 1
 pear install pear.phpunit.de/PHPUnit
+apt-get install git
+git clone https://github.com/sebastianbergmann/phpunit.git
+
+sudo pear channel-discover pear.phpunit.de
+sudo pear channel-discover components.ez.no
+sudo pear channel-discover pear.symfony-project.com
+sudo pear install phpunit/PHP_CodeCoverage
+sudo pear install phpunit/PHPUnit
