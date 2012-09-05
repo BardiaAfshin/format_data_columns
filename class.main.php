d5cbbee60e65bb89ab96292e156083c6030188bc
unknown <brandonusa@gmail.com>
2012-09-05 16:00:50 -0700

    updated class.main.php to meet requirements 1

--- class.main.php
+++ class.main.php
@@ -5,13 +5,12 @@ require_once('./classes/class.parser.php');
 $myParser = new parser('./data/w_data.dat');
 $returned_data = $myParser->get_parsed_data();
 
-print_r($returned_data);
+echo $returned_data[0][1];
 
-foreach($returned_data as $row)
-{
-    foreach($row as $column)
-    {
-        echo $column;
-    }
-    echo "\n";
-}
+echo "\t";
+
+echo $returned_data[0][3];
+
+echo "\t";
+
+echo $returned_data[0][5];
