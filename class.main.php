5e781ebd1b165969e150282d549755f49f7fe834
unknown <brandonusa@gmail.com>
2012-09-05 15:53:30 -0700

    data file name updated

--- class.main.php
+++ class.main.php
@@ -2,7 +2,7 @@
 
 require_once('./classes/class.parser.php');
 
-$myParser = new parser('./data/w_data.data');
+$myParser = new parser('./data/w_data.dat');
 $returned_data = $myParser->get_parsed_data();
 
 print_r($returned_data);
